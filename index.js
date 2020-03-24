ff04df93a00b417b76f3817dfce2a75b6388113a
HeadMad <kov.ps@ya.ru>
2020-03-25 00:19:46 +0800

    commit

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-
 const postcss = require('postcss')
-const globalStyles = require('postcss-svelte-global-styles')
 const path = require('path')
-const fs = require('fs')
+let PLUGINS = [], OPTIONS = {from: undefined}
 
-// const config = getConfigFromPakage('postcss')
-// console.log(config)
+let packagePath = path.resolve(__dirname.substring(0, __dirname.indexOf("node_modules")), 'package.json')
+const package = require(packagePath)
+ 
 
+if (package.postcss && package.postcss.plugins) {
+  const plugins = package.postcss.plugins
+  for (key in plugins) {
+    PLUGINS.push(require(key)(plugins[key]))
+  }
+}
 
+const style = async ({content}) => {
+  console.log({PLUGINS, OPTIONS})
+  if (!PLUGINS || !PLUGINS.length) return
+  let code
+
+  await postcss(PLUGINS)
+    .process(content, OPTIONS)
+    .then(result => {
+      result.warnings().forEach(warn => console.warn(warn.toString()))
+      code = result.css
+    })
+  return { code }
+}
 
 const sveltePostcssPlugin = (plugins = []) => {
-  const style = async ({content, attributes, filename}) => {
-    if (attributes.global) plugins.push(globalStyles)
-    if (!plugins.length) return
-    let code
-
-    await postcss(plugins)
-      .process(content, {from: undefined})
-      .then(result => {
-        result.warnings().forEach(warn => console.warn(warn.toString()))
-        code = result.css
-      })
-    return { code }
-  }
+  if (Array.isArray(plugins))
+    PLUGINS = plugins
   return {style}
 }
+
+sveltePostcssPlugin.style = style
 
 module.exports = sveltePostcssPlugin
